b4398bf6de34dcce07806fb27823f3b4649c55c8
lobinioh <info@wm-studio78.de>
2026-03-19 14:57:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 from app import app
 
-server = app.server  # wichtig!
+server = app.server
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
