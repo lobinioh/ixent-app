0fd9f23e9adaa47fa80b1f235dadf0fc7e5198dd
lobinioh <info@wm-studio78.de>
2026-03-19 14:58:50 +0100

    Update main.py

--- plotly-app/main.py
+++ plotly-app/main.py
@@ -1,8 +1,9 @@
 import os
 from app import app
+from waitress import serve
 
 server = app.server
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    serve(server, host="0.0.0.0", port=port)
