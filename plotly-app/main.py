c6290a7b6774af0da4aca22f677bf3b80a5e176d
lobinioh <info@wm-studio78.de>
2026-03-19 13:49:05 +0100

    Update main.py

--- plotly-app/main.py
+++ plotly-app/main.py
@@ -3,7 +3,4 @@ import waitress
 from app import app
 
 port = int(os.environ.get("PORT", 8050))
-
-print(f"Starting server on port {port}...")
-
 waitress.serve(app.server, host="0.0.0.0", port=port)
